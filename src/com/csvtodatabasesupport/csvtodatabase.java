334e8bdf32dadc2197c398247a9c690ce8d7274e
Tejeswara Rao <50865973+tavvatejeswararaotavva@users.noreply.github.com>
2023-11-03 16:19:54 +0530

    Update csvtodatabase.java

--- src/com/csvtodatabasesupport/csvtodatabase.java
+++ src/com/csvtodatabasesupport/csvtodatabase.java
@@ -21,48 +21,72 @@ public class csvtodatabase {
 
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection(jdbcUrl, username, password);
-			connection.setAutoCommit(false);
+			 connection= DriverManager.getConnection(jdbcUrl,username,password);
+            connection.setAutoCommit(false);
 
-			String sql = "insert into itemlist(id,name,quantity,cost)values(?,?,?,?)";
+            String sql="insert into itemslist(id,name,quantity,cost)values(?,?,?,?)";
+            String updateSql = "UPDATE itemslist SET name = ?, quantity = ?, cost = ? WHERE id = ?";
 
-			PreparedStatement statement = connection.prepareStatement(sql);
 
-			BufferedReader lineReader = new BufferedReader(new FileReader(filePath));
+            PreparedStatement statement=connection.prepareStatement(sql);
+            
+            PreparedStatement updatestatement=connection.prepareStatement(updateSql);
+            
+            
 
-			String lineText = null;
-			int count = 0;
+            BufferedReader lineReader=new BufferedReader(new FileReader(filePath));
 
-			lineReader.readLine();
-			while ((lineText = lineReader.readLine()) != null) {
-				String[] data = lineText.split(",");
+            String lineText=null;
+            int count=0;
 
-				String id = data[0];
-				String name = data[1];
-				String quantity = data[2];
-				String cost = data[3];
+            lineReader.readLine();
+            while ((lineText=lineReader.readLine())!=null){
+                String[] data =lineText.split(",");
+                String id = data[0];
+                String name = data[1];
+                String quantity = data[2];
+                String cost = data[3];
+                
+                if (recordExists(connection, parseInt(id))) {
+                    // Prepare an update operation
+                    updatestatement.setString(1, name);
+                    updatestatement.setString(2, quantity);
+                    updatestatement.setString(3, cost);
+                    updatestatement.setInt(4, parseInt(id));
+                    updatestatement.addBatch();
+                } else {
+                    // Prepare an insert operation
+                    statement.setInt(1, parseInt(id));
+                    statement.setString(2, name);
+                    statement.setString(3, quantity);
+                    statement.setString(4, cost);
+                    statement.addBatch();
+                }
 
-				statement.setInt(1, parseInt(id));
-				statement.setString(2, name);
-				statement.setInt(3, parseInt(quantity));
-				statement.setInt(4, parseInt(cost));
-				statement.addBatch();
+                if(count % batchSize == 0){
+                    statement.executeBatch();
+                    updatestatement.executeBatch();
+                }
+            }
+            lineReader.close();
+            
+            statement.executeBatch();
+            updatestatement.executeBatch();
+            connection.commit();
+            connection.close();
 
-				if (count % batchSize == 0) {
-					statement.executeBatch();
-				}
-			}
-			lineReader.close();
-
-			statement.executeBatch();
-			connection.commit();
-			connection.close();
-
-			System.out.println("Data has been inserted successfully");
+            System.out.println("Data has been inserted successfully");
 
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
 
 	}
+	private static boolean recordExists(Connection connection, int id) throws Exception {
+        String selectSql = "SELECT id FROM itemslist WHERE id = ?";
+        PreparedStatement selectStatement = connection.prepareStatement(selectSql);
+        selectStatement.setInt(1, id);
+        ResultSet resultSet = selectStatement.executeQuery();
+        return resultSet.next();
+    }
 }
